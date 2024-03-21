7a1a88b749b49029b971287a2e51735c80882a64
Garrett Ross <garrettross@gGarrettRoss.attlocal.net>
2024-03-20 19:19:15 -0500

    Added config for database

--- server.js
+++ server.js
@@ -1,14 +1,22 @@
 const express = require('express')
+const dbConfig = require('./db/dbConfig')
 
+const {Pool} = require('pg')
+const dotenv = require('dotenv')
+
+
+dotenv.config()
 const app = express()
-
 const PORT = process.env.PORT || 3000
 
-app.get('/', async(req, res, next) => {
+const pool = new Pool(dbConfig)
+
+app.get('/', async (req, res, next) => {
     try {
-        
+        const result = await pool.query('SELECT * FROM todos;')
+        res.json(result.rows)
     } catch (error) {
-        
+        console.error(error.message)
     }
 })  
 
